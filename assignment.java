681cc57e3135f51cf85a74234ed9355b1f903e6a
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-10 00:13:55 +0530

    Create assignment.java

---
+++ assignment.java
@@ -0,0 +1,15 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a=23;
+        int b;
+        b=a;
+        System.out.println(b);
+        a+=b;
+        System.out.println(a);
+        a-=b;
+        System.out.println(a);
+        a%=b;
+        System.out.println(a);
+    }
+    
+}
