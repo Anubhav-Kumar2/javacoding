ecdb216e1b1ea8fddeae6a7fdc66fd44e044835b
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-10 00:16:59 +0530

    Create arithmatic.java

---
+++ arithmatic.java
@@ -0,0 +1,72 @@
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         int x=4,y=3;
+//         int sum=x+y;
+//         System.out.println(sum);
+//     }
+    
+// }
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         int x=4,y=3;
+//         int subtract=x-y;
+//         System.out.println(subtract);
+//     }
+    
+// }
+
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         int x=4,y=3;
+//         int product=x*y;
+//         System.out.println(product);
+//     }
+    
+// }
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         int x=4,y=3;
+//         int division=x/y;
+//         System.out.println(division);
+//     }
+    
+// }
+
+
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         double x=4,y=3;
+//         double division=x/y;
+//         System.out.println(division);
+//     }
+    
+// }
+
+
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         float x=4,y=3;
+//         float division=x/y;
+//         System.out.println(division);
+//     }
+    
+// }
+
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+//         double x=4,y=3;
+//         // double division=x/y;
+//         System.out.println(x/y);
+//     }
+    
+// }
+
+
+
+
