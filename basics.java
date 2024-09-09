4dcfa3336f2d9bc28cc3599b221db5948759b48e
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-10 00:18:14 +0530

    Create basics.java

---
+++ basics.java
@@ -0,0 +1,297 @@
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println("My name is anubhav");
+//         System.out.print("My name is anubhav");
+//
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println("4");
+//     }
+// }
+
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println(4);
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println("4+3");
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println(4+3);
+//         System.out.println(4*3);
+//         System.out.println(4-3);
+//         System.out.println(4/3);
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int x=56;  // OR int x;
+//                    //x=56;
+//         System.out.println(x);
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int a=6;
+//         //System.out.println(a)
+//         System.out.println(a);
+//     }
+// }
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int a=6,b=4,c=7,d=8,e=5;
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//         System.out.println(d);
+//         System.out.println(e);
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         System.out.println("mast cheez"+5+6+7);
+//     }
+// }
+
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int x=4,y=5,z=8;
+//         System.out.println(x+""+y+z);
+//         System.out.println(x+y+""+z);
+//         System.out.println(x+y+z+"T");
+//     }
+
+// }
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int x=56;
+//         System.out.println(x);
+//         x=x+4;
+//         System.out.println(x);
+//     }
+// }
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int x=6;
+//         x++;
+//         System.out.println(x);
+//         ++x;
+//         System.out.println(x);
+//         x--;
+//         System.out.println(x);
+//         --x;
+//         System.out.println(x);
+//     }
+// }
+
+// public class basics{
+//     public static void main(String[] args) {
+//         int x=6;
+//         // System.out.println(x++);
+//        System.out.println(++x);
+//     }
+// }
+
+
+// import java.util.*;
+// public class basics {
+
+//     public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         System.out.println("enter age:");
+//         int age=s.nextInt();
+//         int humanyear=age*7;
+//         System.out.println("Your age in dog years "+humanyear);
+//         s.close();
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter length");
+//         int length=sc.nextInt();
+//         System.out.println("enter width");
+//         int width=sc.nextInt();
+//         int area=length*width;
+//         System.out.println("area of rectangle is "+area);
+//         sc.close();
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         if(a>0){System.out.println("positive no.");}
+//         else if(a==0){System.out.println("enter zreo");}
+//         else if(a<0){System.out.println("negative no.");}
+//         sc.close();
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter celcius");
+//         int celcius=sc.nextInt();
+//         int f=(celcius*9/5)+32;
+//         System.out.println(f);
+//         sc.close();
+//         }
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         System.out.println("enter b:");
+//         int b=sc.nextInt();
+//         System.out.println("enter c:");
+//         int c=sc.nextInt();
+//         int avg=(a+b+c)/3;
+//         System.out.println(avg);
+//         sc.close();
+
+//     }
+    
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter no:");
+//         int a=sc.nextInt();
+//         int square=a*a;
+//         System.out.println("the square of a is: "+square);
+//         sc.close();
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter no1:");
+//         int a=sc.nextInt();
+//         System.out.println("enter no2:");
+//         int b=sc.nextInt();
+//         int result=a%b;
+//         System.out.println("the result is :"+result);
+//         sc.close();
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter hour");
+//         int a=sc.nextInt();
+//         int hour=60*a;
+//         System.out.println("total minutes :"+hour);
+//         sc.close();
+    
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         System.out.println("enetr b");
+//         int b=sc.nextInt();
+//          a=a^b;
+//          System.out.println("after swapping"+a);
+//          sc.close();
+
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter week");
+//         int a=sc.nextInt();
+//         int week=a*7;
+//          System.out.println("the no.of days :"+week);
+//          sc.close();
+
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         if (a%400==0 || a%4==0);
+//         {System.out.println("leap year");}
+//         // else {
+//         //     System.out.println("not a lepa year");
+        
+//          sc.close();
+
+//     }
+// }
+
+
+
