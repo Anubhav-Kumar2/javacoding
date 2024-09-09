ac1686577478652afec3f0848b3be93af6e2cfef
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-10 00:18:55 +0530

    Create ifelsesheet.java

---
+++ ifelsesheet.java
@@ -0,0 +1,163 @@
+// import java.util.*;
+// public class ifelsesheet {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int  a=sc.nextInt();
+//         System.out.println("enter b:");
+//         int b=sc.nextInt();
+//         if (a>b){System.out.println("a is greater");}
+//         else if(b>a){System.out.println("b is greater");}
+//         sc.close();
+        
+//     }
+    
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         System.out.println("enter b:");
+//         int b=sc.nextInt();
+//         System.out.println("enter c:");
+//         int c=sc.nextInt();
+//         if (a>b && a>c){System.out.println("a is greater");}
+//         else if (b>c){System.out.println("b is greater");}
+//         else{
+//             System.out.println("c is greater");
+//             sc.close();
+//         }
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         if(a>0){System.out.println("a is positive");}
+//         else if(a==0){System.out.println("a is zero");}
+//         else if (a<0){System.out.println("a is negative");}
+//         sc.close();
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         String result;
+//         result=(a%5==0 && a%11==0)?"divisible":"not divisible";
+//         System.out.println(result);
+//         sc.close();
+
+//        //by ternary operator     
+        
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         String result;
+//         result=(a%2==0)?"even":"odd";
+//         System.out.println(result);
+//         sc.close();
+
+//        //by ternary operator     
+        
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         String result;
+//         result=(a%400==0 || a%4==0)?"leap year":"not a leap year";
+//         System.out.println(result);
+//         sc.close();
+
+//        //by ternary operator     
+        
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter a:");
+//         int a=sc.nextInt();
+//         if(a%400==0 || a%4==0){
+//             System.out.println("leap year");
+
+//         }
+//         else{System.out.println("not a leap year");}
+//         sc.close();
+
+//        //by if else     
+        
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class ifelsesheet{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter alphabet");
+//         String chr = sc.next();
+//         if (chr=="a" || chr=="e" || chr=="i" && chr=="o" && chr=="u" ){
+//             System.out.println(chr+ "it is a vowel");
+//         }
+//         else {
+//             System.out.println(" : consonant");
+//             sc.close();
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
