269fe8abda8dba0c8de62fb138e128d340562474
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-10 00:14:45 +0530

    Create logical.java

---
+++ logical.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class logical {
+    public static void main(String[] args) {
+        int a=96,b=26,c=56;
+        System.out.println((a>b)&&(a>c));
+        System.out.println((b<c)||(c>a));
+        System.out.println(!(a==b));
+    }
+    
+}
